554daf83cac72205db773ab9734883529c5eb95e
Alexander Vieira da Silva <alexander.vieira@mgn.local>
2019-06-18 17:38:18 -0300

    Implementing AutoMapper

---
+++ src/Application/WebAPI/AutoMapper/AutoMapperDomainToDtoProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace WebAPI.AutoMapper
+{
+    public class AutoMapperDomainToDtoProfiles:Profile
+    {
+        public AutoMapperDomainToDtoProfiles()
+        {
+            //CreateMap<User, UserDto>();
+        }
+
+        public override string ProfileName {
+
+            get { return "DomainToDto"; }
+
+        }        
+    }
+}
---
+++ src/Application/WebAPI/AutoMapper/AutoMapperDtoToDomainProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace WebAPI.AutoMapper
+{
+    public class AutoMapperDtoToDomainProfiles:Profile
+    {
+        public AutoMapperDtoToDomainProfiles()
+        {
+            //CreateMap<UserDto, User>();
+        }
+
+        public override string ProfileName {
+
+            get { return "DtoToDomain"; }
+
+        }
+    }
+}
